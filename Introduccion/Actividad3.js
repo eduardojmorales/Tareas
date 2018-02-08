b18564ee0235f612a30cb79a441d4de1c1295a6e
Edgar <edgar.jocol@galileo.edu>
2018-02-08 12:43:34 -0600

    Agregado de Codigo

---
+++ Introduccion/Actividad3.js
@@ -0,0 +1,15 @@
+var mes= 10;
+
+if (mes>=13)
+ {alert ("El mes ingresado no es válido!!!");}
+
+if (mes== 12 || mes == 1 || mes==2 )
+   {alert("Es Invierno");}
+  if (mes>= 3 && mes<=5)
+      {alert ("Es Primavera");}
+    if (mes>=6 && mes<=8)
+        {alert ("Es Verano");}
+      if (mes>=9 && mes<=11)
+        {alert ("Es Otoño");}
+   
+{alert ("Edgar Eduardo Jocol Morales, Carne:18007489");}
